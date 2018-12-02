6642dce22b5fa39bb2fc0afe2e6dc4131427c834
Mário Ferreira <marioferreira2110@gmail.com>
2018-12-02 18:41:34 +0100

    Adding code for line chart

---
+++ graphics_test/frmMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace graphics_test
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.crtHeartBeats = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.crtHeartBeats)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // crtHeartBeats
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.crtHeartBeats.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.crtHeartBeats.Legends.Add(legend2);
+            this.crtHeartBeats.Location = new System.Drawing.Point(2, 2);
+            this.crtHeartBeats.Name = "crtHeartBeats";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.crtHeartBeats.Series.Add(series2);
+            this.crtHeartBeats.Size = new System.Drawing.Size(797, 439);
+            this.crtHeartBeats.TabIndex = 0;
+            this.crtHeartBeats.Text = "chart1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(681, 133);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.crtHeartBeats);
+            this.Name = "frmMain";
+            this.Text = "Graphics";
+            ((System.ComponentModel.ISupportInitialize)(this.crtHeartBeats)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart crtHeartBeats;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ graphics_test/frmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphics_test
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        double function(double x) {
+            return (Math.Pow(x, 2) + 2 * Math.Sin(2 * x));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Sets the chart's type
+            crtHeartBeats.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // Alows the user to zoom
+            crtHeartBeats.ChartAreas[0].CursorX.IsUserEnabled = true;
+            crtHeartBeats.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            crtHeartBeats.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            // Removes background lines from the lines chart
+            crtHeartBeats.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            crtHeartBeats.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+
+            for (int i = -15; i < 50; i++)
+            {
+                crtHeartBeats.Series[0].Points.AddXY(i, function(i));
+            }
+        }
+    }
+}
